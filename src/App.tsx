b4be969be04f1370ed32b003946d05327c33dc50
Jelani Canty <jelanic@spotify.com>
2023-12-02 02:17:36 -0500

    miso

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import miso from './images/miso.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. YOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={miso} className="App-logo" alt="miso" />
       </header>
     </div>
   );
