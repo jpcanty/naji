fd9efd7594778b50ee25ead18a94b6235dbd2426
Jelani Canty <jelanic@spotify.com>
2023-12-03 15:20:36 -0500

    better styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
--- src/navigation/FullscreenNav.tsx
+++ src/navigation/FullscreenNav.tsx
@@ -1,4 +1,5 @@
 import { ExIcon } from "./ExIcon";
+import { navigationLinks } from "./navigationLinks";
 
 type Props = {
   onClick: () => void;
@@ -8,12 +9,15 @@ export const FullscreenNav = ({ onClick }: Props) => {
   return (
     <div className="fullscreen">
       <div className="nav-container">
-        <div className="nav-left">\
-        </div>
+        <div className="nav-left" />
         <div className="nav-right">
-          <ExIcon onClick={onClick}/>
+          <ExIcon onClick={onClick} />
         </div>
       </div>
+
+      {navigationLinks.map((link) => {
+            return <p><a className="nav-link" href={link.href}>{link.title}</a></p>;
+          })}
     </div>
   );
 };
